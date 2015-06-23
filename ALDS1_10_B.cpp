4c5df9672e7071a76c3b4ff8b94f53d25afc7820
ottatiyarou <ottatiyarou@gmail.com>
2015-06-24 02:43:18 +0900

    fixed

--- ALDS1_10_B.cpp
+++ ALDS1_10_B.cpp
@@ -17,7 +17,6 @@ int main() {
   for (int l = 2; l <= n; l++) {
     for (int i = 1; i <= n -l + 1; i++) {
       int j = i + l - 1;
-      m[i][j] = i + l -1;
       m[i][j] = (1 << 21);
       for (int k = i; k <= j -1; k++) {
         m[i][j] = min(m[i][j], m[i][k] + m[k+1][j] + p[i-1] * p[k] * p[j]);
